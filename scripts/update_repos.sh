d5bcfd930a9232f399a64684fddc73debe60c538
Đorđe Bjelajac <djordje.bjelajac@packmatic.io>
2024-07-11 21:17:43 +0200

    feat: Update `update_repos.sh` script to accept a base directory as an argument

--- scripts/update_repos.sh
+++ scripts/update_repos.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-# List of directories
-directories=(
-  ##
-)
+
+# Check if an argument is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <path_to_repositories_folder>"
+    exit 1
+fi
+
+# Assign the provided argument to a variable
+base_directory=$1
 
 # Function to determine the default branch
 get_default_branch() {
     git remote show origin | grep 'HEAD branch' | cut -d' ' -f5
 }
 
-# Loop through each directory
-for dir in "${directories[@]}"
-do
+# Find directories within the provided base directory and loop through each
+for dir in "$base_directory"/*; do
     if [ -d "$dir" ]; then
         echo "Entering directory: $dir"
         cd "$dir" || { echo "Failed to enter directory: $dir"; continue; }
@@ -28,8 +32,8 @@ do
         fi
 
         cd - > /dev/null || { echo "Failed to return to previous directory"; exit 1; }
-    else
-        echo "Directory $dir does not exist."
+        else
+            echo "Directory $dir does not exist."
     fi
 done
 
